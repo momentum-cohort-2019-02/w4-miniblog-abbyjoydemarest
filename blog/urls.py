f3da70bf78af7749054f994527467383ec09d523
Abby Demarest <abbyjoydemarest@gmail.com>
2019-03-10 19:59:15 -0400

    set up base_generic.html to draw all the other ones from

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    #create the url path for the index page for the application
+    path('', views.index, name='index'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from blog.models import Blog, Blogger
 # Create your views here.
+
+def index(request):
+    """This is the view function for the homepage/index page of the site."""
+
+
+    #Generate counts of the main objects so people can see what the website contains
+    num_blogs = Blog.objects.all().count()
+    num_bloggers = Blogger.objects.all().count()
+
+
+    #create the context to be displayed later
+    context = {
+        'num_blogs': num_blogs,
+        'num_bloggers': num_bloggers,
+    }
+
+
+    #render the html template  index.html with the context data
+    return render(request, 'index.html', context=context)
